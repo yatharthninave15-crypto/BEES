b2e937c83eedbae6c693982b051b9cf2d29a742d
Yatharth Ninave <yatharthninave15@gmail.com>
2026-06-29 09:25:32 +0530

    Add GitHub Actions deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({
 
-  base: '/BEES/',
+export default defineConfig({
   plugins: [react()],
+  base: 'BEES', // REPLACE with your repository name
 })
